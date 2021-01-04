f49d9e3f844037817967921ee698f5422a96758f
DIR\himanshu.kumar.sinha <himanshu.kumar.sinha@accenture.com>
2021-01-04 12:54:11 +0530

    first commit

---
+++ migctlversion.sh
@@ -0,0 +1,3 @@
+gcloud container clusters get-credentials ansible-demo-cluster --zone us-central1-c --project acn-hybridcloudapplications
+migctl version
+migctl source create ce my-ce-src --project acn-hybridcloudapplications

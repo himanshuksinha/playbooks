4f3e75b9b0085294ce456513547e61dd2bdb2715
167021 <himanshu.sinha09@gmail.com>
2021-01-04 14:15:00 +0530

    Update migrationscript.sh

--- migrationscript.sh
+++ migrationscript.sh
@@ -1,4 +1,5 @@
 gcloud container clusters get-credentials pr-migration-cluster --zone us-east1-b --project acn-hybridcloudapplications
+yum install wget
 wget https://anthos-migrate-release.storage.googleapis.com/v1.5.0/linux/amd64/migctl
 sudo cp migctl /usr/local/bin/
 sudo chmod +x /usr/local/bin/migctl

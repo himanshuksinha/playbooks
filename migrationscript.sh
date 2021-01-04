8b1512eb823bc29a8ea5ce546f8cabe99a5f175a
167021 <himanshu.sinha09@gmail.com>
2021-01-04 16:39:56 +0530

    Update migrationscript.sh

--- migrationscript.sh
+++ migrationscript.sh
@@ -1,5 +1,5 @@
 gcloud container clusters get-credentials pr-migration-cluster --zone us-east1-b --project acn-hybridcloudapplications
-yum install wget
-wget https://anthos-migrate-release.storage.googleapis.com/v1.5.0/linux/amd64/migctl
-cp migctl /usr/local/bin/
-chmod +x /usr/local/bin/migctl
+gcloud services enable servicemanagement.googleapis.com servicecontrol.googleapis.com cloudresourcemanager.googleapis.com container.googleapis.com compute.googleapis.com containerregistry.googleapis.com
+gcloud iam service-accounts create m4a-442817906
+gcloud projects add-iam-policy-binding acn-hybridcloudapplications --member=serviceAccount:m4a-442817906@acn-hybridcloudapplications.iam.gserviceaccount.com --role=roles/storage.admin
+gcloud iam service-accounts keys create sa.json --iam-account=m4a-442817906@acn-hybridcloudapplications.iam.gserviceaccount.com --project acn-hybridcloudapplications

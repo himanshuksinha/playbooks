0c2ecfdfd6e260014072bd436de95d9c2d941b1f
167021 <himanshu.sinha09@gmail.com>
2021-01-11 14:54:18 +0530

    Update migctl-get-migration-artifacts.sh

--- migctl-get-migration-artifacts.sh
+++ migctl-get-migration-artifacts.sh
@@ -1,3 +1,3 @@
 gcloud config set project acn-hybridcloudapplications
 gcloud container clusters get-credentials pr-migration-cluster-1 --zone us-central1-a --project acn-hybridcloudapplications
-migctl migration get-artifacts migration-1 -d /home/himanshu_kumar_sinha/generated
+migctl migration get-artifacts migration-1 -d /home/aritfacts

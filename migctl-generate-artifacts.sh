3eeb25bbf43682870743a9f26acdbcc39ad398bc
167021 <himanshu.sinha09@gmail.com>
2021-01-11 13:34:58 +0530

    Create migctl-generate-artifacts.sh

---
+++ migctl-generate-artifacts.sh
@@ -0,0 +1,3 @@
+gcloud config set project acn-hybridcloudapplications
+gcloud container clusters get-credentials pr-migration-cluster-1 --zone us-central1-a --project acn-hybridcloudapplications
+migctl migration generate-artifacts migration-1

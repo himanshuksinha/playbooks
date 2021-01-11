d6ad54cf97697c4c261261e4539a8be4f518a381
167021 <himanshu.sinha09@gmail.com>
2021-01-11 12:01:06 +0530

    Update migrationscript.sh

--- migrationscript.sh
+++ migrationscript.sh
@@ -1,3 +1,3 @@
 gcloud config set project acn-hybridcloudapplications
 gcloud container clusters get-credentials pr-migration-cluster-1 --zone us-central1-a --project acn-hybridcloudapplications
-migctl setup install --json-key=/home/himanshu_kumar_sinha/m4a-sa-8jan.json
+migctl setup install --json-key=home/himanshu_kumar_sinha/m4a-sa-8jan.json

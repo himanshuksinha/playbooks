19805916202b0b59c0ed2e37da94e7608f7eab0d
167021 <himanshu.sinha09@gmail.com>
2021-01-11 12:52:48 +0530

    Delete installMigctl.sh

--- installMigctl.sh
+++
@@ -1,3 +0,0 @@
-gcloud config set project acn-hybridcloudapplications
-gcloud container clusters get-credentials pr-migration-cluster-1 --zone us-central1-a --project acn-hybridcloudapplications
-migctl setup install --json-key=/home/m4a-sa-8jan.json

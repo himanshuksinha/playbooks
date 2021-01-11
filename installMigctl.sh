92c15a1406e65ff0d83875b3a47df703d4a071e3
167021 <himanshu.sinha09@gmail.com>
2021-01-11 12:50:13 +0530

    Create installMigctl.sh

---
+++ installMigctl.sh
@@ -0,0 +1,3 @@
+gcloud config set project acn-hybridcloudapplications
+gcloud container clusters get-credentials pr-migration-cluster-1 --zone us-central1-a --project acn-hybridcloudapplications
+migctl setup install --json-key=/home/m4a-sa-8jan.json

6d7c09e78fc80cc05cd3a7c9b41af59f36da0c14
167021 <himanshu.sinha09@gmail.com>
2021-01-13 12:00:11 +0530

    Update migctl-create-migration-plan.sh

--- migctl-create-migration-plan.sh
+++ migctl-create-migration-plan.sh
@@ -1,3 +1,3 @@
 gcloud config set project acn-hybridcloudapplications
 gcloud container clusters get-credentials pr-migration-cluster-1 --zone us-central1-a --project acn-hybridcloudapplications
-migctl migration create migration-1 --source source-1 --vm-id webapp-src-vm --intent Image
+migctl migration create migration-1 --source source-2 --vm-id webapp-src-vm --intent Image
